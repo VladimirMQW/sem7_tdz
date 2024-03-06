d1b7a8d24586d8d5ce389ad0ff78586a6ce30433
vmqw <vmqwf@yandex.ru>
2024-03-06 21:07:00 +0300

    lesson1:task2

--- lesson1/Task1/Program.cs
+++ lesson1/Task1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
+//использовала команду: dotnet new gitignore
+//появился файл gitignor, по содержанию сильно похожий на тот, который он показал
 int value = 100;
 Console.WriteLine(value);
-
 double d=5.55;
 char c='!';
 bool b=true;
