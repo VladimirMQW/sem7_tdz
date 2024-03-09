626dd3bd453ffdfb38030351a43684920eed1f70
vmqw <vmqwf@yandex.ru>
2024-03-09 13:24:53 +0300

    add sem1_t1 code

--- sem1_t1/Program.cs
+++ sem1_t1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Является ли первое число квадратом 2го.
+// a=25  b=5  yea
+// a=2 b=10 no
+// a=9 b=-3 yes
+// a=-3 b=9 no
+
+Console.WriteLine("Введите 1е число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2е число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine("Первое является квадратом второго"); 
+}
+else
+{
+    Console.WriteLine("Первое не является квадратом второго"); 
+}
