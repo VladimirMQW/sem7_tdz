e4f6b531d4e8e02dd8655acd2d73031763a12415
vmqw <vmqwf@yandex.ru>
2024-03-09 13:56:18 +0300

    add sem1_t1 code2

--- sem1_t1/Program.cs
+++ sem1_t1/Program.cs
@@ -4,7 +4,7 @@
 // a=2 b=10 no
 // a=9 b=-3 yes
 // a=-3 b=9 no
-/*
+
 Console.WriteLine("Введите 1е число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -18,25 +18,4 @@ if (num1 == num2*num2)
 else
 {
     Console.WriteLine("Первое не является квадратом второго"); 
-}
-*/
-// Задача2
-//Принимаем целое число N и показываем все целые от -N до N.
-// 4  -4, -3, -2, -1, 0, 1, 2, 3, 4
-
-
-Console.WriteLine("Введите целое положительное число:");
-int n = Convert.ToInt32(Console.ReadLine());
-if (n > 0)
-{
-    int i = -n;
-    while (i <= n)
-    {
-        Console.Write(i + ", ");
-        i = i + 1;
-    }
-}
-else
-{
-    Console.WriteLine("Некорректный ввод");
 }
