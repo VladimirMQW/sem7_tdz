ea6398ab2f9ad0f5a0388c7134d81ca6a064b8d7
vmqw <vmqwf@yandex.ru>
2024-03-09 14:16:29 +0300

    add sem1_t3 code

--- sem1_t3/Program.cs
+++ sem1_t3/Program.cs
@@ -3,3 +3,19 @@
 // 456  -- 10
 // 782 -- 9
 //918 -- 17
+
+
+Console.WriteLine("Введите целое 3хЗначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num >= 100 && num <= 999) || (num >= -999 && num <= -100))
+{
+    int digitFirst = num / 100;
+    int digitLast = num % 10;
+    int sum = digitFirst + digitLast;
+    Console.WriteLine("Сумма: " + sum);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
