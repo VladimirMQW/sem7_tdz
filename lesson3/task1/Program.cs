e49a7a47cda91971662604387ba70efa1c89e4ad
vmqw <vmqwf@yandex.ru>
2024-03-17 23:46:47 +0300

    add lesson3:task1

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalcFormula(int a, int b, int c, int d)
+{
+    double num = a * b;
+    int denom = c + d;
+    double result = num / denom;
+    //double result = a * b / (c + d);
+    return result;
+}
+
+double res = CalcFormula(1, 2, 3, 4);
+
+Console.WriteLine(res);
